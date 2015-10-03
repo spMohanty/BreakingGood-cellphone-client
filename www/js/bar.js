08b4f37f0695d24fcb77f935b685e25b6f32d54b
Amit Navindgi <navindgi@usc.edu>
2015-10-02 18:19:46 -0700

    trail

---
+++ www/js/bar.js
@@ -0,0 +1,6 @@
+cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
