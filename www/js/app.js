1e20c4e2b1a6280b4eda8a069add4b37e58c1c47
Amit Navindgi <navindgi@usc.edu>
2015-10-04 03:58:28 -0700

    login

--- www/js/app.js
+++ www/js/app.js
@@ -20,40 +20,7 @@ function startScan() {
 			//$('#results').load('test.html');
 			resultDiv.innerHTML = s;
 
-			example = "https://api.twitter.com/1.1/statuses/show.json";
-
-			info = {
-				med_id : s;
-			};
-
-			// var xhttp = new XMLHttpRequest();
-			// xhttp.onreadystatechange = function() {
-			//     if (xhttp.readyState == 4 && xhttp.status == 200) {
-			// 	      resultDiv.innerHTML = xhttp.responseText;
-			// 	    }
-			// 	}
-			// }
-			// xhttp.open("GET", "http://breaking-good-map.cern.ch:5601/check_validity", true);
-			// xhttp.send();
-
-			// $.ajax({
-		 //         type: "GET",
-		         
-		 //         url: "http://breaking-good-map.cern.ch:5601/check_validity",
-		         
-		 //         data : info,
-
-
-		 //         success: function (data, status, jqXHR) {
-		 //             // do something
-		 //             alert("success");
-		 //         },
-
-		 //         error: function (jqXHR, status) {
-		 //             // error handler
-		 //             alert("failure");
-		 //         }
- 		// 	});
+			
 		}, 
 		function (error) {
 			alert("Scanning failed: " + error);
