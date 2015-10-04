f434cb869fab68ee47836fea4ef88caf7b2ac813
Amit Navindgi <navindgi@usc.edu>
2015-10-04 07:11:37 -0700

    login

--- www/js/app.js
+++ www/js/app.js
@@ -15,38 +15,48 @@ function init() {
 
 
 function startScan() {
-	cordova.plugins.barcodeScanner.scan(
-		function (result) {
-			var s = "Result: " + result.text + "<br/>" +
-			"Format: " + result.format + "<br/>" +
-			"Cancelled: " + result.cancelled;
-			//$('#results').load('test.html');
-			//resultDiv.innerHTML = s;
-			medid = "http://breaking-good-map.cern.ch:5000/check_validity?med_id=" + result.text;
 
-
-			resultDiv.innerHTML = "Welcome" + result.text + "!";
-
-			$.support.cors=true;
-			$.get(medid, function(data){
-				resultDiv.innerHTML += "Working !!";
-			})
-
-		}, 
-		function (error) {
-			alert("Scanning failed: " + error);
-		}
-	);
-}
-
-$(document).ready(function(){
 			medid = "http://breaking-good-map.cern.ch:5000/check_validity?med_id=BLEH !!!"
 
 
-			resultDiv.innerHTML += "<br/>TEST CALL Welcome" + result.text + "!";
+			resultDiv.innerHTML += "<br/>TEST CALL Welcome!";
 
 			$.support.cors=true;
 			$.get(medid, function(data){
 				resultDiv.innerHTML += "TEST CALL Working !!";
-			})
+			})	
+	// cordova.plugins.barcodeScanner.scan(
+	// 	function (result) {
+	// 		var s = "Result: " + result.text + "<br/>" +
+	// 		"Format: " + result.format + "<br/>" +
+	// 		"Cancelled: " + result.cancelled;
+	// 		//$('#results').load('test.html');
+	// 		//resultDiv.innerHTML = s;
+	// 		medid = "http://breaking-good-map.cern.ch:5000/check_validity?med_id=" + result.text;
+
+
+	// 		resultDiv.innerHTML = "Welcome" + result.text + "!";
+
+	// 		$.support.cors=true;
+	// 		$.get(medid, function(data){
+	// 			resultDiv.innerHTML += "Working !!";
+	// 		})
+
+	// 	}, 
+	// 	function (error) {
+	// 		alert("Scanning failed: " + error);
+	// 	}
+	// );
+}
+
+$(document).ready(function(){
+			// medid = "http://breaking-good-map.cern.ch:5000/check_validity?med_id=BLEH !!!"
+
+
+			// resultDiv.innerHTML += "<br/>TEST CALL Welcome" + result.text + "!";
+
+			// $.support.cors=true;
+			// $.get(medid, function(data){
+			// 	resultDiv.innerHTML += "TEST CALL Working !!";
+			// })
 });
